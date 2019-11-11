33b4d01094ae141fc2b1f2c0f08c8f25846e4488
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-11 13:52:31 +0000

    set up users model

--- Users/user-model.js
+++ Users/user-model.js
@@ -1,7 +1,25 @@
-const db = require('../Database/dbConfig')
-
+const db = require("../Database/dbConfig");
 
 module.exports = {
-    // find,
-    // findBy
+  find,
+  findById,
+  add
+};
+
+function find() {
+  return db("users").select("id", "username", "password");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function add(user) {
+  db("Users")
+    .insert(user, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
 }
