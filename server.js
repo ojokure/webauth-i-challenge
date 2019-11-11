993effb3ed476bc7c7814bd29422ca372e2c2841
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-11 13:06:36 +0000

    set up server with index.js and server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const server = express;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send('<h1> YO! </h1>')
+})
