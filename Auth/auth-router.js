25687426d822c0f62ab6eccf865ed79b9a55b74e
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-12 14:13:48 +0000

    set up session

--- Auth/auth-router.js
+++ Auth/auth-router.js
@@ -37,6 +37,7 @@ authRouter.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome back ${user.username}!` });
       } else {
         res
@@ -45,8 +46,10 @@ authRouter.post("/login", (req, res) => {
       }
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({
+        message: error.message
+      });
     });
 });
 
-module.exports = authRouter
+module.exports = authRouter;
--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
-const authRouter = require('./Auth/auth-router');
-const usersRouter = require('./Users/user-router');
-
-const Users = require("./Users/user-model");
+const authRouter = require("./Auth/auth-router");
+const usersRouter = require("./Users/user-router");
 
 const server = express();
+
+const sessionConfig = {
+  name: "cookieCookie",
+  secret: "open sesame",
+  cookie: {
+    maxAge: 1000 * 60 * 35,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require("./Database/dbConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 35
+  })
+};
 
 server.use(helmet());
 server.use(cors());
 server.use(cookieParser());
 server.use(express.json());
+server.use(session(sessionConfig));
 
-
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1> YO! </h1>");
 });
-
-
 
 module.exports = server;
